492e22e38903ea1448c2b269e9405343faba5265
Martin Van_Raay <martinvraay@gmail.com>
2026-02-11 12:41:47 -0500

    created basic backend logic for adding friends

--- src/main/java/com/accountabilibuddy/api/controller/FriendController.java
+++ src/main/java/com/accountabilibuddy/api/controller/FriendController.java
@@ -1,13 +1,28 @@
 package com.accountabilibuddy.api.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.accountabilibuddy.api.service.FriendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
     private final FriendService _service;
+
+    @GetMapping
+    public String getMyFriends(Model model, Principal principal) {
+        // TODO: Retrieve user's friends
+        return "friends"; // TODO: Create friends page
+    }
+
+    @PostMapping
+    public String addFriend(Principal principal, @RequestParam Long friendId){
+        _service.addFriend(principal, friendId);
+        return "redirect:/friends";
+    }
 }
--- src/main/java/com/accountabilibuddy/api/service/FriendService.java
+++ src/main/java/com/accountabilibuddy/api/service/FriendService.java
@@ -5,14 +5,23 @@ import com.accountabilibuddy.api.repository.FriendRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FriendService {
     private final FriendRepository _repo;
+    private final UserService _userService;
 
     public List<Friend> findAll() {
         return _repo.findAll();
+    }
+
+    public Friend addFriend(Principal principal, Long friendId) {
+        Friend friend = new Friend();
+        friend.setFriendOneId(_userService.getCurrentUserId(principal));
+        friend.setFriendTwoId(friendId);
+        return _repo.save(friend);
     }
 }
