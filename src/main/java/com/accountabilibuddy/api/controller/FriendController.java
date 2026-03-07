2948aea9b7c365b719cc6684aae8a7073b38767a
Ryan <r1d8o0o9@gmail.com>
2026-03-07 13:37:18 -0500

    view friend goals

--- src/main/java/com/accountabilibuddy/api/controller/FriendController.java
+++ src/main/java/com/accountabilibuddy/api/controller/FriendController.java
@@ -1,24 +1,38 @@
 package com.accountabilibuddy.api.controller;
 
 import com.accountabilibuddy.api.dto.UserSearchDto;
+import com.accountabilibuddy.api.model.Goal;
+import com.accountabilibuddy.api.model.User;
 import com.accountabilibuddy.api.service.FriendService;
+import com.accountabilibuddy.api.service.GoalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
     private final FriendService _service;
+    private final GoalService _goalService;
 
     @GetMapping
     public String getMyFriends(Model model, Principal principal) {
-        model.addAttribute("friends", _service.getFriendsForCurrentUser(principal));
+        List<User> friends = _service.getFriendsForCurrentUser(principal);
+
+        Map<Long, List<Goal>> friendGoals = new HashMap<>();
+        for (User friend : friends) {
+            friendGoals.put(friend.getId(), _goalService.getGoalsForUser(friend.getId()));
+        }
+
+        model.addAttribute("friends", friends);
+        model.addAttribute("friendGoals", friendGoals);
         return "friends";
     }
 
